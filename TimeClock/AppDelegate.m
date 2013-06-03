4ffe65755f77d81f5e2bc7c69ba4efb4a45d0d2b
Sylvain Fay-Chatelard <sylvain.faychatelard@gmail.com>
2013-06-03 17:48:06 +0200

    Call the viewDidLoad of the current view controller when application become active

--- TimeClock/AppDelegate.m
+++ TimeClock/AppDelegate.m
@@ -78,7 +78,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [[AppDelegate topMostController] viewWillAppear:YES];
+    [[AppDelegate topMostController] viewDidAppear:YES];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
